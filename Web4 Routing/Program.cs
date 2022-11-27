63e4d2368f445acc00bf14003a68434a98758d53
G7M7T1 <84965845+G7M7T1@users.noreply.github.com>
2022-11-27 11:44:32 -0500

    Update File

--- Web4 Routing/Program.cs
+++ Web4 Routing/Program.cs
@@ -1,3 +1,5 @@
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -17,6 +19,49 @@ app.UseEndpoints(endpoints =>
         string? extension = Convert.ToString(context.Request.RouteValues["extension"]);
 
         await context.Response.WriteAsync($"{fileName} - {extension}");
+    });
+
+
+    // default value
+    endpoints.Map("person/{id=1}", async context =>
+    {
+        int num = Convert.ToInt32(context.Request.RouteValues["id"]);
+
+        await context.Response.WriteAsync($"You Are In Person {num}");
+    });
+
+
+    // if value
+    endpoints.Map("doc/{id?}", async context =>
+    {
+        if(context.Request.RouteValues.ContainsKey("id"))
+        {
+            int num = Convert.ToInt32(context.Request.RouteValues["id"]);
+
+            await context.Response.WriteAsync($"You Are In Doc {num}");
+        } 
+        
+        else
+        {
+            await context.Response.WriteAsync($"You Are In DOC");
+        }
+    });
+
+
+    // int value
+    endpoints.Map("num/{id:int?}", async context =>
+    {
+        if (context.Request.RouteValues.ContainsKey("id"))
+        {
+            int num = Convert.ToInt32(context.Request.RouteValues["id"]);
+
+            await context.Response.WriteAsync($"You Are In num {num}");
+        }
+
+        else
+        {
+            await context.Response.WriteAsync($"You Are In num /");
+        }
     });
 });
 
