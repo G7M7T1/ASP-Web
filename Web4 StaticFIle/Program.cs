71b31d923e9a1ff1f3c19663c644b05b50eba965
G7M7T1 <84965845+G7M7T1@users.noreply.github.com>
2022-11-29 18:38:19 -0500

    Update

--- Web4 StaticFIle/Program.cs
+++ Web4 StaticFIle/Program.cs
@@ -1,6 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+// https://localhost:7046/logo.png
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.Map("/", async context => { await context.Response.WriteAsync("Hello Index"); });
+});
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
