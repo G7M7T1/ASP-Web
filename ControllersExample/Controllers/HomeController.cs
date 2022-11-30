bb4eaf79fda965dabb7d451e2e0fabec6c36c622
G7M7T1 <84965845+G7M7T1@users.noreply.github.com>
2022-11-29 22:34:45 -0500

    Update

--- ControllersExample/Controllers/HomeController.cs
+++ ControllersExample/Controllers/HomeController.cs
@@ -2,11 +2,41 @@
 
 namespace ControllersExample.Controllers
 {
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
+    /*    public class HomeController : Controller
         {
-            return View();
+            public IActionResult Index()
+            {
+                return View();
+            }
+        }*/
+
+    [Controller]
+    class HomeController: Microsoft.AspNetCore.Mvc.Controller // Home
+    {
+        [Route("/")]
+        [Route("home")]
+        [Route("index")]
+        public string Index()
+        {
+            return "Hello From Index Page";
+        }
+
+
+        [Route("about")]
+        public string About()
+        {
+            return "Hello From About Page";
+        }
+
+
+        [Route("content")]
+        public ContentResult Content()
+        {
+            return new ContentResult()
+            {
+                Content = "content",
+                ContentType = "text/html"
+            };
         }
     }
 }
