92342da4d6eaa73631dd6e694c3b4c918257a1c8
G7M7T1 <84965845+G7M7T1@users.noreply.github.com>
2022-11-29 19:40:00 -0500

    Update

--- Web4 StaticFIle/Program.cs
+++ Web4 StaticFIle/Program.cs
@@ -1,14 +1,35 @@
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+// If you need change to other root folder
+/*var builder = WebApplication.CreateBuilder(new WebApplicationOptions()
+{
+    WebRootPath = "myroot"
+});*/
+
+
 var app = builder.Build();
 
 // https://localhost:7046/logo.png
 app.UseStaticFiles();
+
+
+// If you need more root folder to give static file
+app.UseStaticFiles(new StaticFileOptions()
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(builder.Environment.ContentRootPath, "otherroot"))
+});
+
 
 app.UseRouting();
 
 app.UseEndpoints(endpoints =>
 {
     endpoints.Map("/", async context => { await context.Response.WriteAsync("Hello Index"); });
+
 });
 
 app.MapGet("/", () => "Hello World!");
