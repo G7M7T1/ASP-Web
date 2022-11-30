ea8687e84efd70a7b41b304bde8cf3a9bcf644b6
G7M7T1 <84965845+G7M7T1@users.noreply.github.com>
2022-11-29 20:59:46 -0500

    Update

--- ControllersExample/Program.cs
+++ ControllersExample/Program.cs
@@ -1,6 +1,25 @@
+using ControllersExample.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add single Controller
+// builder.Services.AddTransient<HomeController>();
+
+// Add All Controller
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+/*// Old Way
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    // active all controller method
+    endpoints.MapControllers();
+});*/
+
+// new way
+app.MapControllers();
 
 app.Run();
