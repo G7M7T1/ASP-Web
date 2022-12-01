c6e31e9fcd9df41f784c918b31c248e2b9982957
G7M7T1 <84965845+G7M7T1@users.noreply.github.com>
2022-12-01 11:34:32 -0500

    Update File

--- IActionResultExample/Controllers/HomeController.cs
+++ IActionResultExample/Controllers/HomeController.cs
@@ -9,15 +9,19 @@ namespace IActionResultExample.Controllers
         {
             if (!Request.Query.ContainsKey("bookid"))
             {
-                Response.StatusCode = 400;
-                return Content("Book Id is not supplied");
+                // Response.StatusCode = 400;
+                // return Content("Book Id is not supplied");
+
+                // return new BadRequestResult();
+                return BadRequest("Book Id is not supplied");
             }
 
             // Book Id can't be empty
             if (string.IsNullOrEmpty(Convert.ToString(Request.Query["bookid"])))
             {
-                Response.StatusCode = 404;
-                return Content("Book Id can't be null or empty");
+                // Response.StatusCode = 404;
+                // return Content("Book Id can't be null or empty");
+                return BadRequest("Book Id can't be null or empty");
             }
 
             // Book Id should be between 1 to 1000
@@ -25,15 +29,28 @@ namespace IActionResultExample.Controllers
 
             if (bookId <= 0)
             {
-                return Content("Book Id Can't be less than 0");
+                // return Content("Book Id Can't be less than 0");
+                return NotFound("Book Id Can't be less than 0");
             }
 
             if (bookId > 1000)
             {
-                return Content("Book Id Can't be bigger than 1000");
+                // return Content("Book Id Can't be bigger than 1000");
+
+                return NotFound("Book Id Can't be bigger than 1000");
             }
 
-            return File("text.pdf", "application/pdf");
+            // return File("text.pdf", "application/pdf");
+
+            // return new RedirectToActionResult("Books", "Store", new {});
+
+            // send Id
+            // return RedirectToAction("Books", "Store", new { id = bookId });
+
+            return RedirectToActionPermanent("Books", "Store", new { id = bookId });
+
+            // moved Permanently
+            // return new RedirectToActionResult("Books", "Store", new {}, permanent: true);
         }
     }
 }
---
+++ IActionResultExample/Controllers/StoreController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IActionResultExample.Controllers
+{
+    public class StoreController : Controller
+    {
+        [Route("store/books")]
+        public IActionResult Books()
+        {
+            // Get Id
+            int id = Convert.ToInt32(Request.RouteValues["id"]);
+
+            return Content($"Hello Books {id}", "text/html");
+        }
+    }
+}
