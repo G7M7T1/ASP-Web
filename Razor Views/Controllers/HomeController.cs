1d2bb3422f0b685fdb771fb1e3be6bb2b25f4110
G7M7T1 <84965845+G7M7T1@users.noreply.github.com>
2022-12-06 01:11:02 -0500

    Update

---
+++ Razor Views/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Razor_Views.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
